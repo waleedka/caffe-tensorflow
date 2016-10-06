e623b8d76229e9dccea9cffb36244a8865a1b218
Yangyan Li <yangyan.lee@gmail.com>
2016-10-05 20:05:30 -0700

    Small changes to match with tensorflow r0.11 API.

--- examples/imagenet/dataset.py
+++ examples/imagenet/dataset.py
@@ -20,7 +20,7 @@ def process_image(img, scale, isotropic, crop, mean):
         new_shape = tf.to_int32((scale / min_length) * img_shape)
     else:
         new_shape = tf.pack([scale, scale])
-    img = tf.image.resize_images(img, new_shape[0], new_shape[1])
+    img = tf.image.resize_images(img, size=[new_shape[0], new_shape[1]])
     # Center crop
     # Use the slice workaround until crop_to_bounding_box supports deferred tensor shapes
     # See: https://github.com/tensorflow/tensorflow/issues/521
